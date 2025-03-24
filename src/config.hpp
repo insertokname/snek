7011ffd98cc6b6ee49e57191946ae4e24380a338
insertokername <111150085+insertokername@users.noreply.github.com>
2025-03-24 17:14:52 +0200

    changed speed config

--- src/config.hpp
+++ src/config.hpp
@@ -4,11 +4,17 @@
 namespace snek {
     constexpr std::size_t INITIAL_SCREEN_WIDTH = 1280,
                           INITIAL_SCREEN_HEIGHT = 720,
-                          BOARD_HEIGHT = 10, BOARD_WIDTH = 20,
-                          MOVE_SPEED = 250000;
+                          BOARD_HEIGHT = 10, BOARD_WIDTH = 20;
 #ifdef __EMSCRIPTEN__
     constexpr std::size_t PADDING = 0;
 #else
     constexpr std::size_t PADDING = 75;
 #endif
+
+#ifdef SNEK_ALGORITHM
+    constexpr std::size_t MOVE_SPEED = 50000;
+#else
+    constexpr std::size_t MOVE_SPEED = 200000;
+#endif
+
 }
