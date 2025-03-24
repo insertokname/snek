0aca722bf59de5392c035db3a3f9da06299f9880
insertokname <insertokname@gmail.com>
2025-03-24 11:34:52 +0200

    padding preferences on wasm

--- src/config.hpp
+++ src/config.hpp
@@ -3,7 +3,12 @@
 #include <cstddef>
 namespace snek {
     constexpr std::size_t INITIAL_SCREEN_WIDTH = 1280,
-                          INITIAL_SCREEN_HEIGHT = 720, PADDING = 75,
+                          INITIAL_SCREEN_HEIGHT = 720, 
                           BOARD_HEIGHT = 10, BOARD_WIDTH = 20,
                           MOVE_SPEED = 250000;
+#ifdef __EMSCRIPTEN__
+    constexpr std::size_t PADDING = 0;
+#else
+    constexpr std::size_t PADDING = 75;
+#endif
 }
