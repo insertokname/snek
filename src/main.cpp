e143aa67f06433940c9cf7f8f7461bdc2fb2fdff
insertokername <111150085+insertokername@users.noreply.github.com>
2025-03-24 17:28:24 +0200

    attemtpt to fix keyboards on algo

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@ int main(int argc, char *argv[]) {
     SDL_EventState(SDL_TEXTINPUT, SDL_DISABLE);
     SDL_EventState(SDL_KEYDOWN, SDL_DISABLE);
     SDL_EventState(SDL_KEYUP, SDL_DISABLE);
+#ifdef __EMSCRIPTEN__
+    SDL_SetHint(SDL_HINT_EMSCRIPTEN_KEYBOARD_ELEMENT, "#document");
+#endif
 #endif
     snek::App app;
     app.run();
