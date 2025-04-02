efec42ca7b510e2beea702346f81e99b8bd1fd4e
insertokname <111150085+insertokname@users.noreply.github.com>
2025-04-02 07:54:51 +0000

    Deploying to gh-pages from @ insertokname/snek@fd50c6a61b075c41e1c941fd8ba13b1b3e428b84 🚀

--- game/snek.js
+++ game/snek.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpqgko5zt9.js
+// include: /tmp/tmpko80k273.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -219,21 +219,21 @@ Module['FS_createPath']("/", "assets", true, true);
 
   })();
 
-// end include: /tmp/tmpqgko5zt9.js
-// include: /tmp/tmp_sjps29v.js
+// end include: /tmp/tmpko80k273.js
+// include: /tmp/tmpmtbr71i_.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp_sjps29v.js
-// include: /tmp/tmp638qfol1.js
+  // end include: /tmp/tmpmtbr71i_.js
+// include: /tmp/tmp0uqv53qm.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp638qfol1.js
+  // end include: /tmp/tmp0uqv53qm.js
 
 
 // Sometimes an existing Module object exists with properties
