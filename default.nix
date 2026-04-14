f387bb4cc6c5b1749241e13513ea4772338d79e0
insertokername <insertokname@gmail.com>
2026-04-14 20:29:10 +0300

    nix font fix

--- default.nix
+++ default.nix
@@ -1,18 +1,22 @@
 # Stolen from here: https://github.com/nixvital/nix-based-cpp-starterkit
 
 { lib, llvmPackages_16, cmake, SDL2, SDL2_ttf, clang-tools, game }:
-llvmPackages_16.stdenv.mkDerivation rec {
+llvmPackages_16.stdenv.mkDerivation {
   pname = "snek";
   version = "0.1.0";
 
   src = ./.;
 
   nativeBuildInputs = [ cmake clang-tools ];
-  buildInputs = [ SDL2 ];
+  buildInputs = [ SDL2 SDL2_ttf ];
 
   cmakeFlags = [ "-DENABLE_TESTING=OFF" "-DENABLE_INSTALL=ON" ]
     ++ lib.optional game "-DSNEK_ALGORITHM=ON";
 
   SDL2TTF_DIR = "${SDL2_ttf}";
   SDL2_DIR = "${SDL2.dev}";
+
+  postInstall = ''
+    cp -r $src/assets $out/bin/assets
+  '';
 }
