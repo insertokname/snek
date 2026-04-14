41e60ffa39373cb8c328ff2def3253feeadb7198
insertokername <insertokname@gmail.com>
2026-04-14 22:21:42 +0300

    added a small perf script

---
+++ perf.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/env fish
+
+test (basename $PWD) != build && cd ./build
+
+rm -rf * \
+    && cmake .. \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+        -DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer" \
+        -DSNEK_ALGORITHM=ON \
+        -DSNEK_PERF=ON \
+    && cmake --build . --parallel \
+    && time perf record -F 99 --call-graph dwarf -- ./src/snek \
+    && perf script | stackcollapse-perf.pl | flamegraph.pl > flame.svg
