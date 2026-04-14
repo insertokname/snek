9789ab5758e9d04579f648fed1b3603afcdebf80
insertokname <111150085+insertokname@users.noreply.github.com>
2026-04-14 19:24:43 +0000

    Deploying to gh-pages from @ insertokname/snek@41e60ffa39373cb8c328ff2def3253feeadb7198 🚀

--- algorithm/snek.js
+++ algorithm/snek.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmphuzw3obo.js
+// include: /tmp/tmpujmls58_.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -219,21 +219,21 @@ Module['FS_createPath']("/", "assets", true, true);
 
   })();
 
-// end include: /tmp/tmphuzw3obo.js
-// include: /tmp/tmpbaoiljdf.js
+// end include: /tmp/tmpujmls58_.js
+// include: /tmp/tmp8bcm4po1.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpbaoiljdf.js
-// include: /tmp/tmprab8vx22.js
+  // end include: /tmp/tmp8bcm4po1.js
+// include: /tmp/tmp7tiwwsys.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmprab8vx22.js
+  // end include: /tmp/tmp7tiwwsys.js
 
 
 // Sometimes an existing Module object exists with properties
